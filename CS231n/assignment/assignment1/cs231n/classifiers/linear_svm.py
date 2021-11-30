70a20a0d60398393598c86ea15692b59892c36aa
haochengxia <xhc_1007@163.com>
2021-11-30 21:57:24 +0800

    cs231n: assignment1 add some notes

--- CS231n/assignment/assignment1/cs231n/classifiers/linear_svm.py
+++ CS231n/assignment/assignment1/cs231n/classifiers/linear_svm.py
@@ -36,6 +36,8 @@ def svm_loss_naive(W, X, y, reg):
             margin = scores[j] - correct_class_score + 1 # note delta = 1
             if margin > 0:
                 loss += margin
+                # *** My Code Begin ***
+                # Use this margin to calculate the gradient  
 
     # Right now the loss is a sum over all training examples, but we want it
     # to be an average instead so we divide by num_train.
@@ -54,7 +56,7 @@ def svm_loss_naive(W, X, y, reg):
     #############################################################################
     # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
-    pass
+    dW = 
 
     # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
     
--- CS231n/note/assignment1.md
+++ CS231n/note/assignment1.md
@@ -21,7 +21,21 @@ Standard Deviation，即为标准差。
 
 ## 2 训练一个支持向量机
 
+求梯度矩阵$dW$：
 
+$ \nabla_W L_i$ = $\frac{\partial L_i}{\partial W}$
+
+同时结合样本$X_i$的损失函数计算公式如下：
+
+$Li = \sum_{j \neq y_i} max(0, W_j^T \cdot X_i - W_{y_i}^T \cdot X_i + \Delta)$
+
+由此可知其在不同分类方向上的梯度：
+
+1. 在正确分类上的梯度为：
+   $$ \nabla_{W_{y_i}} L_i = -$$
+
+
+ $\partial $
 
 ## 3 实现一个Softmax分类器
 
